cdfc45a451b90e701240b5fda400a727aaaa8ca1
Loïc Rouquette <loic.rouquette@insa-lyon.fr>
2021-03-04 14:59:28 +0100

    Rename blockSize to textSize

--- src/main/kotlin/com/github/rloic/phd/core/cryptography/rijndael/differential/relatedkey/step1/enumeration/Configuration.kt
+++ src/main/kotlin/com/github/rloic/phd/core/cryptography/rijndael/differential/relatedkey/step1/enumeration/Configuration.kt
@@ -5,26 +5,26 @@ import com.github.rloic.phd.core.cryptography.rijndael.checkKeySize
 import com.github.rloic.phd.core.cryptography.rijndael.checkNr
 import com.github.rloic.phd.core.cryptography.rijndael.checkObjStep1
 
-open class Configuration(val Nr: Int, val blockSize: Int, val keySize: Int, val objStep1: Int) {
+open class Configuration(val Nr: Int, val textSize: Int, val keySize: Int, val objStep1: Int) {
 
-    val Nb: Int get() = blockSize / 32
+    val Nb: Int get() = textSize / 32
     val Nk: Int get() = keySize / 32
 
     fun isSbColumn(ik: Int) =  ik >= Nk - 1 && (ik != Nb * (Nr + 1) - 1) && ((ik % Nk == Nk - 1) || (Nk > 6 && ik % Nk == 3))
 
     init {
         checkNr(Nr)
-        checkBlockSize(blockSize)
+        checkBlockSize(textSize)
         checkKeySize(keySize)
         checkObjStep1(objStep1)
     }
 
     fun copy(
         Nr: Int = this.Nr,
-        blockSize: Int = this.blockSize,
+        blockSize: Int = this.textSize,
         keySize: Int = this.keySize,
         objStep1: Int = this.objStep1
     ) = Configuration(Nr, blockSize, keySize, objStep1)
 
-    override fun toString() = "Configuration(Nr=$Nr, blockSize=$blockSize, keySize=$keySize, objStep1=$objStep1)"
+    override fun toString() = "Configuration(Nr=$Nr, textSize=$textSize, keySize=$keySize, objStep1=$objStep1)"
 }
--- src/main/kotlin/com/github/rloic/phd/core/cryptography/rijndael/differential/relatedkey/step1/optimization/Configuration.kt
+++ src/main/kotlin/com/github/rloic/phd/core/cryptography/rijndael/differential/relatedkey/step1/optimization/Configuration.kt
@@ -4,21 +4,21 @@ import com.github.rloic.phd.core.cryptography.rijndael.checkBlockSize
 import com.github.rloic.phd.core.cryptography.rijndael.checkKeySize
 import com.github.rloic.phd.core.cryptography.rijndael.checkNr
 
-open class Configuration(val Nr: Int, val blockSize: Int, val keySize: Int) {
+open class Configuration(val Nr: Int, val textSize: Int, val keySize: Int) {
 
-    val Nb: Int get() = blockSize / 32
+    val Nb: Int get() = textSize / 32
     val Nk: Int get() = keySize / 32
 
     fun isSbColumn(ik: Int) =  ik >= Nk - 1 && (ik != Nb * (Nr + 1) - 1) && ((ik % Nk == Nk - 1) || (Nk > 6 && ik % Nk == 3))
 
     init {
         checkNr(Nr)
-        checkBlockSize(blockSize)
+        checkBlockSize(textSize)
         checkKeySize(keySize)
     }
 
-    fun copy(Nr: Int = this.Nr, blockSize: Int = this.blockSize, keySize: Int = this.keySize) =
+    fun copy(Nr: Int = this.Nr, blockSize: Int = this.textSize, keySize: Int = this.keySize) =
         Configuration(Nr, blockSize, keySize)
 
-    override fun toString() = "Configuration(Nr=$Nr, blockSize=$blockSize, keySize=$keySize)"
+    override fun toString() = "Configuration(Nr=$Nr, textSize=$textSize, keySize=$keySize)"
 }

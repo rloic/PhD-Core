9625ab4def2ae313260031eee4a2cf370b8d98bd
Loïc Rouquette <loic.rouquette@insa-lyon.fr>
2021-10-15 11:50:43 +0200

    Add mzn arguments

--- src/main/kotlin/com/github/rloic/phd/core/mzn/MznSolver.kt
+++ src/main/kotlin/com/github/rloic/phd/core/mzn/MznSolver.kt
@@ -5,16 +5,16 @@ import com.github.rloic.phd.core.mzn.utils.ForbidPreviousPartialSolutionIterator
 
 interface MznSolver {
 
-    fun optimize(model: MznModel.Optimization, data: Map<String, Any>): MznSolution?
-    fun solveOnce(model: MznModel.PartialSearch, data: Map<String, Any>): MznSolution?
-    fun solveOnce(model: MznModel.CompleteSearch, data: Map<String, Any>): MznSolution?
-    fun enumerate(model: MznModel.PartialSearch, data: Map<String, Any>): Iterator<MznSolution> = makeIterator(model, data)
-    fun enumerate(model: MznModel.CompleteSearch, data: Map<String, Any>): Iterator<MznSolution> = makeIterator(model, data)
+    fun optimize(model: MznModel.Optimization, data: Map<String, Any>, vararg mznArgs: String): MznSolution?
+    fun solveOnce(model: MznModel.PartialSearch, data: Map<String, Any>, vararg mznArgs: String): MznSolution?
+    fun solveOnce(model: MznModel.CompleteSearch, data: Map<String, Any>, vararg mznArgs: String): MznSolution?
+    fun enumerate(model: MznModel.PartialSearch, data: Map<String, Any>, vararg mznArgs: String): Iterator<MznSolution> = makeIterator(model, data)
+    fun enumerate(model: MznModel.CompleteSearch, data: Map<String, Any>, vararg mznArgs: String): Iterator<MznSolution> = makeIterator(model, data)
 
-    fun makeIterator(model: MznModel.CompleteSearch, data: Map<String, Any>): Iterator<MznSolution> =
-        ForbidPreviousCompleteSolutionIterator(this, model, data)
+    fun makeIterator(model: MznModel.CompleteSearch, data: Map<String, Any>, vararg mznArgs: String): Iterator<MznSolution> =
+        ForbidPreviousCompleteSolutionIterator(this, model, data, mznArgs)
 
-    fun makeIterator(model: MznModel.PartialSearch, data: Map<String, Any>): Iterator<MznSolution> =
-        ForbidPreviousPartialSolutionIterator(this, model, data)
+    fun makeIterator(model: MznModel.PartialSearch, data: Map<String, Any>, vararg mznArgs: String): Iterator<MznSolution> =
+        ForbidPreviousPartialSolutionIterator(this, model, data, mznArgs)
 
 }

2a2b08a5238666febe2f624c166e87b401713f84
Loïc Rouquette <loic.rouquette@insa-lyon.fr>
2022-02-09 10:23:40 +0100

    Add format function to MznCompiler

--- src/main/kotlin/com/github/phd/core/mzn/Mzn2FznCompiler.kt
+++ src/main/kotlin/com/github/phd/core/mzn/Mzn2FznCompiler.kt
@@ -1,7 +1,10 @@
 package com.github.phd.core.mzn
 
+import java.io.File
+
 interface Mzn2FznCompiler {
 
+    fun format(mznSolution: MznSolution, oznFile: File): MznSolution
     fun compile(mznModel: MznModel, data: Map<String, Any>, solver: SolverKind, vararg mznArgs: String): FznModel
 
 }
--- src/main/kotlin/com/github/phd/core/mzn/MznModel.kt
+++ src/main/kotlin/com/github/phd/core/mzn/MznModel.kt
@@ -4,6 +4,12 @@ import java.io.File
 
 sealed class MznModel(val value: File) {
 
+    val oznFile get(): File {
+        val path = value.absolutePath
+        if (!path.endsWith(".mzn")) throw Exception("Invalid mzn file extension")
+        return File(path.substring(0, path.length - ".mzn".length) + ".ozn")
+    }
+
     class Optimization(value: File, val objective: MznVariable, val search: OptimizationSearch): MznModel(value)
 
     class CompleteSearch(value: File, val assignment: (MznSolution) -> Assignment.Complete, val forbid: (Assignment.Complete) -> Unit): MznModel(value)
